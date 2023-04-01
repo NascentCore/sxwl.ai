c6cbb2bf345de77dc571938e4f064874bed987c2
zhoujie16 <zhoujie16m@163.com>
2023-04-01 14:47:16 +0800

    initial commit,create home page

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from '../theme';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
