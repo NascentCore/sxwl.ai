9c447254c1b227962fa71d56363ac115633fc698
Yaxiong Zhao <112656580+yaxiong-zhao@users.noreply.github.com>
2023-04-01 12:04:52 +0800

    Rename job in redeploy.sh

--- devops/redeploy.sh
+++ devops/redeploy.sh
@@ -5,7 +5,7 @@ yarn install
 yarn run build
 
 # Restart the mainsite's nodejs server
-job_name="tricorder_dev_site"
+job_name="nascentcore_ai"
 pm2 stop ${job_name}
 pm2 delete ${job_name}
 pm2 start yarn --name ${job_name} -- run start
