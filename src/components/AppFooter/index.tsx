b4d83c60053805fd0ce03981500c5021f6e33309
zhoujie <zhoujie16m@163.com>
2023-07-22 20:13:25 +0800

    add footer (#19)
    <img width="700" alt="截屏2023-06-04 18 12 09"
    src="https://github.com/tricorder-observability/nascentcore.ai/assets/17493672/c74ba81d-eb52-444b-9c36-1fd38b4d7ab0">
    <img width="700" alt="截屏2023-06-04 18 12 26"
    src="https://github.com/tricorder-observability/nascentcore.ai/assets/17493672/b28092f7-609d-4fbf-b3da-236a04db9754">
    
    <img width="700" alt="截屏2023-06-04 18 14 50"
    src="https://github.com/tricorder-observability/nascentcore.ai/assets/17493672/134eec80-04f5-471e-bd56-c6c09d31cef1">
    
    src/components/AppFooter/index.tsx 修改相关内容即可

--- src/components/AppFooter/index.tsx
+++ src/components/AppFooter/index.tsx
@@ -1,62 +1,64 @@
+import Link from "next/link";
 import Logo from "../AppHeader/Logo";
 import Image from "next/image";
 
 /**
- * 公共页面底部（预留组件，方便以后拓展
+ * 公共页面底部
  */
 export default function AppFooter() {
+  // 商务合作邮箱
+  const businessEmail = "bd@deeplang.ai";
+  // 公司地址
+  const companyAddress = "北京市海淀区 清华科技园 威盛大厦";
+  // 简历投递邮箱
+  const resumeEmail = "join@deeplang.ai";
+  // boss招聘主页
+  const bossUrl =
+    "https://www.zhipin.com/gongsi/6d1e4b436619f4df1Xd43di-FFM~.html?ka=search_rcmd_company_name_6d1e4b436619f4df1Xd43di-FFM~_custompage";
+  // 飞书投递简历
+  const feishuUrl = "https://wenjuan.feishu.cn/m/cfm?t=sT0CPNNzNhLi-qegt";
+
   return (
     <>
       <div className="app_footer">
         <div className="app_footer_inner">
           <div className="app_footer_logo">
             <Logo />
-            <div className="company_name">xxx科技有限公司</div>
+            <div className="company_name">
+              <div>本网站由北京算想未来有限责任公司运营</div>
+              <div>©️2023 nascentcore.ai 版权所有</div>
+            </div>
           </div>
           <div className="join_item_wrap">
             <div className="join_item">
-              {/* <div className="join_item_header">加入我们</div> */}
-              {/* <div className="join_item_title">商务邮箱</div>
-              <div className="join_item_text">商务邮箱</div>
+              <div className="join_item_header">加入我们</div>
+              <div className="join_item_title">商务合作邮箱</div>
+              <div className="join_item_text">{businessEmail}</div>
               <div className="join_item_title">公司地址</div>
-              <div className="join_item_text">公司地址</div> */}
-              <div className="join_item_qrcode">
-                <Image
-                  width={200}
-                  height={278}
-                  src="/assets/join_qr.png"
-                  alt=""
-                />
-              </div>
+              <div className="join_item_text">{companyAddress}</div>
             </div>
             <div className="join_item">
-              {/* <div className="join_item_header">加入我们</div> */}
-              {/* <div className="join_item_title">商务邮箱</div>
-              <div className="join_item_text">商务邮箱</div>
-              <div className="join_item_title">公司地址</div>
-              <div className="join_item_text">公司地址</div> */}
-              <div className="join_item_qrcode">
-                <Image
-                  width={200}
-                  height={278}
-                  src="/assets/follow_qr.png"
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className="join_item">
-              {/* <div className="join_item_header">加入我们</div> */}
-              {/* <div className="join_item_title">商务邮箱</div>
-              <div className="join_item_text">商务邮箱</div>
-              <div className="join_item_title">公司地址</div>
-              <div className="join_item_text">公司地址</div> */}
-              <div className="join_item_qrcode">
-                <Image
-                  width={200}
-                  height={278}
-                  src="/assets/wechat_qr.jpg"
-                  alt=""
-                />
+              <div className="join_item_header">加入我们</div>
+              <div className="join_item_title">简历投递邮箱</div>
+              <div className="join_item_text">{resumeEmail}</div>
+              <div className="join_item_icons">
+                <Link href={bossUrl} target="__blank">
+                  <Image
+                    src={"/assets/boss_zhipin.png"}
+                    alt={"进入招聘主页"}
+                    width={42}
+                    height={42}
+                  ></Image>
+                </Link>
+
+                <Link href={feishuUrl} target="__blank">
+                  <Image
+                    src={"/assets/feishu.png"}
+                    alt={"快速投递简历"}
+                    width={42}
+                    height={42}
+                  ></Image>
+                </Link>
               </div>
             </div>
           </div>
