cfbcc1ff0e522d3017044173c222d0810a9ff9b7
zhoujie <zhoujie16m@163.com>
2023-09-15 00:36:56 +0800

    feat: Add internationalization

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -30,7 +30,7 @@ const App: React.FC = () => {
   const onClick: MenuProps["onClick"] = (e) => {
     setCurrent(e.key);
     if (e.key == 1) {
-      window.location.href = "https://nascentcore.ai/cloud";
+      window.location.href = "https://cloud.nascentcore.ai";
     }
   };
 
